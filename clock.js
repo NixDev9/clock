89253d40a1a5c6a481f335c3e7d3aad38bbc0e7d
Nix <134012290+NixDev9@users.noreply.github.com>
2023-07-22 02:16:19 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,8 +2,7 @@
 function getCurrentESTTime() {
   const now = new Date();
   const estOffset = -5 * 60; // EST is UTC-5
-  const estTime = new Date(now.getTime() + estOffset * 60 * 1000);
-  return estTime;
+  return new Date(now.getTime() + estOffset * 60 * 1000);
 }
 
 // Function to format time in 12-hour format with AM/PM
@@ -31,20 +30,41 @@ function formatDate(date) {
   return `${day} ${month} ${year}`;
 }
 
+// Function to get the local time for the visitor based on their time zone
+function getVisitorLocalTime(timeZoneOffset) {
+  const now = new Date();
+  const localOffset = now.getTimezoneOffset();
+  const visitorTimeOffset = localOffset + timeZoneOffset;
+  return new Date(now.getTime() + visitorTimeOffset * 60 * 1000);
+}
+
 // Function to update the clock display
 function updateClock() {
+  // EST time
   const estTime = getCurrentESTTime();
   const timeElement = document.getElementById("time");
   const ampmElement = document.getElementById("ampm");
   const dateElement = document.getElementById("date");
-  const monthElement = document.getElementById("month");
-  const yearElement = document.getElementById("year");
 
   timeElement.textContent = formatTime(estTime);
   ampmElement.textContent = estTime.getHours() >= 12 ? "PM" : "AM";
   dateElement.textContent = formatDate(estTime);
+
+  // Visitor's time
+  const visitorTimeZoneOffset = new Date().getTimezoneOffset();
+  const visitorLocalTime = getVisitorLocalTime(visitorTimeZoneOffset);
+  const visitorTimeElement = document.getElementById("visitorTime");
+
+  visitorTimeElement.textContent = formatTime(visitorLocalTime) + " " + getShortTimeZoneAbbreviation(visitorTimeZoneOffset);
+}
+
+// Function to get short timezone abbreviation
+function getShortTimeZoneAbbreviation(timeZoneOffset) {
+  const offsetHours = Math.abs(Math.floor(timeZoneOffset / 60));
+  const offsetMinutes = Math.abs(timeZoneOffset % 60);
+  const sign = timeZoneOffset > 0 ? "-" : "+";
+  return `GMT ${sign}${offsetHours}:${offsetMinutes}`;
 }
 
 // Run the updateClock function every second
 setInterval(updateClock, 1000);
-
